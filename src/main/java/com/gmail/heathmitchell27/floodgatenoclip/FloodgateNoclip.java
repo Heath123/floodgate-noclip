9e74b871f5a5d71277e8e296f9d8bcc608fa265f
Heath123 <heath.mitchell27@gmail.com>
2020-06-02 19:38:56 +0100

    Initial commit

---
+++ src/main/java/com/gmail/heathmitchell27/floodgatenoclip/FloodgateNoclip.java
@@ -0,0 +1,35 @@
+package com.gmail.heathmitchell27.floodgatenoclip;
+
+import net.minecraft.server.v1_15_R1.DedicatedServerProperties;
+import org.bukkit.craftbukkit.v1_15_R1.CraftServer;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.spigotmc.SpigotConfig;
+
+import java.lang.reflect.Field;
+import java.util.logging.Logger;
+
+public class FloodgateNoclip extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        try {
+            DedicatedServerProperties properties = ((CraftServer) getServer()).getServer().getDedicatedServerProperties();
+            Field allowFlight = properties.getClass().getDeclaredField("allowFlight");
+            allowFlight.setAccessible(true);
+            allowFlight.set(properties, true);
+        } catch (Exception e) {
+            getLogger().warning("floodgate-noclip could not set allow-flight value!");
+            e.printStackTrace();
+        }
+        SpigotConfig.movedTooQuicklyMultiplier = 100;
+        SpigotConfig.movedWronglyThreshold = 100;
+        getLogger().info("Set config options for floodgate-noclip!");
+    }
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new Listener(getLogger()), this);
+        getLogger().info("Enabled floodgate-noclip!");
+        getLogger().warning("Warning: floodgate-noclip disables all vanilla anticheat for Bedrock players and loosens anticheat for Java players! Only use it on private servers.");
+    }
+}
---
+++ src/main/java/com/gmail/heathmitchell27/floodgatenoclip/Listener.java
@@ -0,0 +1,29 @@
+package com.gmail.heathmitchell27.floodgatenoclip;
+
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.geysermc.floodgate.FloodgateAPI;
+
+import java.util.logging.Logger;
+
+public class Listener implements org.bukkit.event.Listener {
+    private final Logger logger;
+
+    Listener (Logger logger) {
+        this.logger = logger;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {
+        Player player = event.getPlayer();
+        // System.out.println(player.getUniqueId());
+        // System.out.println(FloodgateAPI.isBedrockPlayer(player.getUniqueId()));
+        if (FloodgateAPI.isBedrockPlayer(player.getUniqueId())) {
+            ((CraftPlayer) player).getHandle().noclip = true;
+            logger.info("floodgate-noclip: disabled collision for player " + player.getName());
+        }
+    }
+}
